d843bbb411563415b1238e1c02689fa29ce5f18d
David Dassau <todieahero@hotmail.com>
2018-05-24 18:55:48 -0500

    created project and added angularjs route

---
+++ MixedMediaInventoryTracker/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace MixedMediaInventoryTracker.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ MixedMediaInventoryTracker/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MixedMediaInventoryTracker.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
