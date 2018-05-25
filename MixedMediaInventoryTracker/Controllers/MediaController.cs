a3fb00dbe2529a27903ac2784e88f01a117880b7
David Dassau <todieahero@hotmail.com>
2018-05-25 14:23:41 -0500

    post new media item is now working in Postman

--- MixedMediaInventoryTracker/Controllers/MediaController.cs
+++ MixedMediaInventoryTracker/Controllers/MediaController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Net.Http;
 using System.Web;
 using System.Web.Http;
-//using MixedMediaInventoryTracker.Services;
 using MixedMediaInventoryTracker.Models;
 using System.Net;
 
