4b8d27432882020c0dc137d217075e737141be80
David Dassau <todieahero@hotmail.com>
2018-05-25 13:38:49 -0500

    post not working yet, going to try to get the get working

--- MixedMediaInventoryTracker/Controllers/MediaController.cs
+++ MixedMediaInventoryTracker/Controllers/MediaController.cs
@@ -1,40 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
+//using MixedMediaInventoryTracker.Services;
 using MixedMediaInventoryTracker.Models;
-using MixedMediaInventoryTracker.Services;
+using System.Net;
 
 namespace MixedMediaInventoryTracker.Controllers
 {
     [RoutePrefix("api/media")]
     public class MediaController : ApiController
     {
-        [Route(""), HttpPost]
-        public HttpResponseMessage Create(MediaModel media)
+        [HttpPost, Route("")]
+        public HttpResponseMessage CreateMedia(MediaModel media)
         {
             var mediaRepository = new MediaRepository();
             var result = mediaRepository.Create(media);
 
-
+            if (result)
+            {
+                return Request.CreateResponse(HttpStatusCode.Created);
+            }
+            return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Could not create Media item at this time, try again later");
         }
-
-
-        //[Route(""), HttpPost]
-        //public HttpResponseMessage CreateProduct(Product product)
-        //{
-        //    var productRepository = new ProductRepository();
-        //    var result = productRepository.Create(product);
-
-        //    if (result)
-        //    {
-        //        return Request.CreateResponse(HttpStatusCode.Created);
-        //    }
-
-        //    return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Could not create product, try again later");
-        //}
     }
 }
