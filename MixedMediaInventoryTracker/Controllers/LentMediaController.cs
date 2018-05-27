c61fc52162ca219cd10acf9e4463931aa8bc6690
David Dassau <todieahero@hotmail.com>
2018-05-26 20:20:43 -0500

    created files for lent media

---
+++ MixedMediaInventoryTracker/Controllers/LentMediaController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace MixedMediaInventoryTracker.Controllers
+{
+    [RoutePrefix("api/lentmedia")]
+    public class LentMediaController : ApiController
+    {
+        [HttpGet, Route("")]
+
+    }
+}
