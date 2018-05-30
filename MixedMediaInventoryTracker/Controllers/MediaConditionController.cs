f8a0c98010133a617e26af6c85ea294fd7e360d3
David Dassau <todieahero@hotmail.com>
2018-05-30 14:14:51 -0500

    added MediaConditionController

---
+++ MixedMediaInventoryTracker/Controllers/MediaConditionController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using MixedMediaInventoryTracker.Services;
+
+namespace MixedMediaInventoryTracker.Controllers
+{
+    [RoutePrefix("api/mediaCondition")]
+    public class MediaConditionController : ApiController
+    {
+        [HttpGet, Route("")]
+        public HttpResponseMessage GetMediaConditions()
+        {
+            var mediaConditionRepository = new MediaConditionRepository();
+            var allMediaConditions = mediaConditionRepository.GetAllMediaConditions();
+
+            return Request.CreateResponse(HttpStatusCode.OK, allMediaConditions);
+        }
+    }
+}
