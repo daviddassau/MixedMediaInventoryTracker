ed9417a0ee4104660314bf9fe895d68f0fb2dff6
David Dassau <todieahero@hotmail.com>
2018-05-31 15:23:01 -0500

    got media item types to display, but all of them are displaying

--- MixedMediaInventoryTracker/app/controllers/ViewMedia.js
+++ MixedMediaInventoryTracker/app/controllers/ViewMedia.js
@@ -3,6 +3,17 @@
 
         $scope.message = "My Media";
 
+        $scope.mediaItem = {};
+        $scope.types = {};
+
+        $scope.displayMediaType = function (type) {
+            $scope.mediaItem.mediaType = type.mediaType;
+        };
+
+        $http.get("/api/mediaType").then(function (results) {
+            $scope.types = results.data;
+        });
+
         var getAllMediaItems = function () {
             $http.get("/api/media").then(function (result) {
                 $scope.mediaItems = result.data;
