8bef45a9a9828a9308e0d51a6903bf6b4d73e2c7
David Dassau <todieahero@hotmail.com>
2018-06-01 10:19:51 -0500

    got Title showing up on edit item page

--- MixedMediaInventoryTracker/app/controllers/EditMedia.js
+++ MixedMediaInventoryTracker/app/controllers/EditMedia.js
@@ -1,7 +1,21 @@
-﻿app.controller("EditMedia", ["$scope", "$http", "$location",
-    function ($scope, $http, $location) {
+﻿app.controller("EditMedia", ["$scope", "$http", "$location", "$routeParams",
+    function ($scope, $http, $location, $routeParams) {
 
         $scope.message = "Edit Media";
+
+        $scope.editMediaItem = {};
+
+        var getSingleMediaItem = function () {
+            $http.get(`api/media/${$routeParams.id}`).then(function (result) {
+                console.log(result);
+                //result.data.MediaTypeId = new Date(result.data.MediaTypeId);
+                //result.data.MediaConditionId = new Date(result.data.MediaConditionId);
+                $scope.editMediaItem = result.data;
+            });
+        };
+
+        console.log(getSingleMediaItem());
+        getSingleMediaItem();
 
     }
 ]);
