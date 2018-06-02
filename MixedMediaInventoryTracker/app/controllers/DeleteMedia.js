4c671bf64674131a3aa7b5d1e768740180527329
David Dassau <todieahero@hotmail.com>
2018-06-02 12:22:18 -0500

    you can now click on the delete button and it routes you to the page where it asks you if you're sure you'd like to delete that item

--- MixedMediaInventoryTracker/app/controllers/DeleteMedia.js
+++ MixedMediaInventoryTracker/app/controllers/DeleteMedia.js
@@ -1,1 +1,14 @@
-﻿
+﻿app.controller("DeleteMedia", ["$scope", "$http", "$location", "$routeParams",
+    function ($scope, $http, $location, $routeParams) {
+
+        $scope.mediaItem = {};
+
+        $http.get(`api/media/${$routeParams.id}`).then(function (result) {
+            $scope.mediaItem = result.data;
+        });
+
+
+
+    }
+]);
+
