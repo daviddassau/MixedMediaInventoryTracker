279cac64f876fd69ab553ff1a779dbb519936428
David Dassau <todieahero@hotmail.com>
2018-06-04 13:39:59 -0500

    got form fields for lendee name, date lent, and notes

--- MixedMediaInventoryTracker/app/controllers/MarkAsLent.js
+++ MixedMediaInventoryTracker/app/controllers/MarkAsLent.js
@@ -3,7 +3,11 @@
 
         $scope.message = "Mark this item as Lent";
 
-        
+        $scope.mediaItem = {};
+
+        $http.get(`api/media/${$routeParams.id}`).then(function (result) {
+            $scope.mediaItem = result.data;
+        });
 
     }
 ]);
