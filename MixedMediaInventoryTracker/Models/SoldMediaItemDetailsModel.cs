c6d389873d6b3e774ebede24b5b40777ca25c8b9
David Dassau <todieahero@hotmail.com>
2018-06-06 07:39:52 -0500

    added new model

--- MixedMediaInventoryTracker/Models/SoldMediaItemDetailsModel.cs
+++ MixedMediaInventoryTracker/Models/SoldMediaItemDetailsModel.cs
@@ -7,6 +7,12 @@ namespace MixedMediaInventoryTracker.Models
 {
     public class SoldMediaItemDetailsModel
     {
-
+        public int Id { get; set; }
+        public string Buyer { get; set; }
+        public int? Amount { get; set; }
+        public DateTime SoldDate { get; set; }
+        public string Notes { get; set; }
+        public string Title { get; set; }
+        public string MediaCondition { get; set; }
     }
 }
