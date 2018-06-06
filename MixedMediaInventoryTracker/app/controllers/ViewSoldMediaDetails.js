93a34a77f1fb5d1f49b716d319a8bb798deef9b4
David Dassau <todieahero@hotmail.com>
2018-06-06 07:44:50 -0500

    set up partial and controller

--- MixedMediaInventoryTracker/app/controllers/ViewSoldMediaDetails.js
+++ MixedMediaInventoryTracker/app/controllers/ViewSoldMediaDetails.js
@@ -1,1 +1,20 @@
-﻿
+﻿app.controller("ViewSoldMediaDetails", ["$scope", "$http", "$location", "$routeParams", "moment",
+    function ($scope, $http, $location, $routeParams, moment) {
+
+        $scope.message = "Sold Media Details";
+
+        //$scope.itemDetails = {};
+
+        //function getItemDetails() {
+        //    $http.get(`api/lentmedia/lentMediaItemDetails/${$routeParams.id}`).then(function (result) {
+        //        $scope.itemDetails = result.data;
+        //    }).catch(function (error) {
+        //        console.log("error in getItemDetails", error);
+        //    });
+        //}
+
+        //getItemDetails();
+
+    }
+]);
+
