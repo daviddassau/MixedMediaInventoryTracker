712fa8555b757e35fd0ea29b97d3d3956f4251ba
David Dassau <todieahero@hotmail.com>
2018-06-09 13:19:33 -0500

    gathering icons for media types

--- MixedMediaInventoryTracker/app/app.js
+++ MixedMediaInventoryTracker/app/app.js
@@ -11,6 +11,11 @@ app.config(["$routeProvider", function ($routeProvider) {
             templateUrl: "/app/partials/viewMedia.html",
             controller: "ViewMedia"
         })
+        .when("/chooseMediaType",
+        {
+            templateUrl: "/app/partials/chooseMediaType.html",
+            controller: "ChooseMediaType"
+        })
         .when("/viewMedia/:id",
         {
             templateUrl: "/app/partials/viewMediaDetails.html",
---
+++ MixedMediaInventoryTracker/app/controllers/ChooseMediaType.js
@@ -0,0 +1,30 @@
+﻿app.controller("ChooseMediaType", ["$scope", "$http", "$location", "$routeParams", "moment",
+    function ($scope, $http, $location, $routeParams, moment) {
+
+        $scope.message = "Choose The Media Type";
+
+
+
+        //$http.get("/api/media").then(function (results) {
+        //    $scope.mediaItems = results.data;
+        //});
+
+        //$scope.viewMediaItemDetails = function (id) {
+        //    $location.path(`/viewMedia/${id}`);
+        //}
+
+        //$scope.editMediaItem = function (id) {
+        //    $location.path(`/viewMedia/edit/${id}`);
+        //};
+
+        //$scope.lendMediaItem = function (id) {
+        //    $location.path(`/viewLentMedia/${id}`);
+        //}
+
+        //$scope.deleteMediaItem = function (id) {
+        //    $location.path(`/viewMedia/delete/${id}`);
+        //};
+
+    }
+]);
+
